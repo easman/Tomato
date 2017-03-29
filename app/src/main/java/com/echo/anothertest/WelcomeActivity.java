88964c426b1b86ce54c71843a5c9adb4c5291764
easman <easman@sina.com>
2017-03-29 16:42:05 +0800

    Initial commit

--- app/src/main/java/com/echo/anothertest/WelcomeActivity.java
+++ app/src/main/java/com/echo/anothertest/WelcomeActivity.java
@@ -35,7 +35,7 @@ public class WelcomeActivity extends Activity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(WelcomeActivity.this, MainActivity.class);
+                Intent intent = new Intent(WelcomeActivity.this, FaceActivity.class);
                 startActivity(intent);
                 WelcomeActivity.this.finish();
             }
