a892a6527ef7d6c387774db01358ac6b7cb0db95
easman <easman@sina.com>
2017-03-29 16:07:04 +0800

    Initial commit

---
+++ app/src/main/java/com/echo/anothertest/TodoListActivity.java
@@ -0,0 +1,10 @@
+package com.echo.anothertest;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Echo on 2017/3/29.
+ */
+
+public class TodoListActivity extends AppCompatActivity {
+}
